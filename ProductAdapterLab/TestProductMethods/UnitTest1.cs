2d7b1ece0a067759251b52272e5353883f5f08ab
Sasa7 <sasa.rogic@gmail.com>
2014-10-13 13:47:49 +0200

    Created ProductLib + IProduct + ProductClass some impelements IProduct

---
+++ ProductAdapterLab/TestProductMethods/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProductMethods
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_That_ProductProperties_AreNotEmpty()
+        {
+        }
+    }
+}
